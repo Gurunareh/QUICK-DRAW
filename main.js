0a2fd6c83881bc8dc6af850d5102a554d61dd770
Gurunareh <85010055+Gurunareh@users.noreply.github.com>
2022-11-04 20:07:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,36 @@ function setup(){
     canvas= createCanvas(280,280);
     canvas.center()
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+ synth=window.speechSynthesis;
    }
    
    function clearCanvas(){
        background("white")
    }
+   function preload(){
+    classifier=m15.imageClassifier('DoodleNet');
+}
+function draw(){
+}
+// set stroke weight to 13
+strokeweight(13);
+// set stroke color to black
+stroke(0);
+//if mouse is pressed draw line between previous current mouse positions
+if (mouseIsPressed) {
+    line(pmouseX,pmouseY,mouseX,mouseY);
+}
+function classifyCanvas(){
+    clssifier.classify(canvas,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML='label:'+results[0].label;
+    document.getElementById('confidence').innerHTML='cofidence:'+Math.round(results[0].confidence*100)+'%';
+    utterThis-new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
